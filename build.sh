34674dbb3d2e022940607b4550ac1c42cb1ef4f9
高帅朋 <gaoshuaipeng@ecloudinfo.com>
2021-01-05 10:07:33 +0800

    fix

--- build.sh
+++ build.sh
@@ -3,4 +3,9 @@
 #TAG=$(git rev-parse --short HEAD)
 TAG=$(date "+%Y%m%d%H%M%S") \
 && docker build -f Dockerfile.prod -t commonweb_pod:${TAG} . \
+&& docker tag commonweb_pod:${TAG} a112121788/commonweb_pod:${TAG} \
+&& docker push a112121788/commonweb_pod:${TAG} \
 && echo "commonweb_pod:${TAG}"
+
+# docker tag commonweb_pod:20210105091829 a112121788/commonweb_pod:20210105091829
+# docker push a112121788/commonweb_pod:20210105091829
